d471c237a3c539a26ed8f9e045af43822fad5478
rameshbb34 <65815554+rameshbb34@users.noreply.github.com>
2021-09-20 17:53:52 +0530

    Add files via upload

---
+++ 2b.js
@@ -0,0 +1,63 @@
+const users = [
+    {
+        name:'Brook', 
+        scores:75,
+        skills:['HTM', 'CSS', 'JS'],
+        age:16
+    },
+    {
+        name:'Alex', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    }, 
+    {
+        name:'David', 
+        scores:75,
+        skills:['HTM', 'CSS'],
+        age:22
+    }, 
+    {
+        name:'John', 
+        scores:85,
+        skills:['HTM'],
+        age:25
+    },
+    {
+        name:'Sara',
+        scores:95,
+        skills:['HTM', 'CSS', 'JS'],
+        age: 26
+    },
+    {
+        name:'Martha', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    },
+    {
+        name:'Thomas',
+        scores:90,
+        skills:['HTM', 'CSS', 'JS'],
+        age:20
+    }
+    ];
+
+
+
+// Write a function which addUser to the user array only if the user does not exist.
+
+    function addUser(){
+        users.forEach(user => {
+            if(user.name !== 'Raj'){
+                
+                users.name = "Raj",
+                users.scores = 70,
+                users.skills= ['HTM', 'CSS', 'JS'],
+                users.age = 21
+            }
+        });
+         console.log(users);
+    }
+    addUser(users)
+
